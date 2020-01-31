1f57217f6a6fa5bb651f4e3298f908c530f67751
skadro <55257066+skadro-official@users.noreply.github.com>
2020-02-01 00:43:52 +0100

    Create main.cpp

---
+++ files/main.cpp
@@ -0,0 +1,15 @@
+#include "skCrypter.h"
+#include <iostream>
+
+int main()
+{
+	auto testString = skCrypt("TestString");	// encrypted at compile-time
+	printf(testString);							// automatic decryption on usage (alternatevly .decrypt())
+
+	testString.encrypt();						// encrypt after usage if needed again
+											// or
+	testString.clear();							// set full string storage to 0
+
+	getchar();
+	return 0;
+}
